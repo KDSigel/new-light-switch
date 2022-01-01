eb590af83b19d6c43f6bad9da7a193e34f58186e
KDSigel <heykarl@gmail.com>
2022-01-01 14:55:39 -0800

    fixed switcher

--- src/views/LightOn/LightOn.jsx
+++ src/views/LightOn/LightOn.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function LightOn({setLightSwitch}) {
     return (
         <div>
-            test
-            {/* <input type="button" value="Click me" onclick={setLightSwitch(true)} /> */}
+            <input type="button" value="Click me" onClick={() => setLightSwitch(true)} />
         </div>
     )
 }
